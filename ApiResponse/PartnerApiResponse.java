2b109ad2f958659697b1a65a102693c8feae835b
meezzui <86812098+meezzui@users.noreply.github.com>
2021-08-31 05:03:54 +0900

    ApiResponse

---
+++ ApiResponse/PartnerApiResponse.java
@@ -0,0 +1,27 @@
+package com.koreait.day3.model.network.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class PartnerApiResponse {
+    private Long id;
+    private String name;
+    private String status;
+    private String address;
+    private String callCenter;
+    private String businessNumber;
+    private String ceoName;
+    private LocalDateTime regDate;
+    private LocalDateTime updateDate;
+}
