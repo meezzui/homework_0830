697bfedb7f8119737f46aaf032ef22d653c3235e
meezzui <86812098+meezzui@users.noreply.github.com>
2021-08-31 05:07:30 +0900

    js 폴더

---
+++ js폴더/adminuser.js
@@ -0,0 +1,51 @@
+(function ($) {
+    let indexBtn = [];
+    let pagination = {
+        total_pages : 0,
+        total_elements :0,
+        current_page :0,
+        current_elements: 0
+    };
+
+    //페이지 정보
+    let showPage = new Vue({
+        el: "#showPage",
+        data: {
+            totalElements: {},
+            currentPage: {}
+        }
+    });
+
+    // 데이터 리스트
+    let itemList = new Vue({
+        el : '#itemList',
+        data : {
+            itemList : {}
+        }
+    });
+
+    //html을 다 읽으면 동작해라(이 페이지에서 가장 먼저 실행됨)
+    $(document).ready(function(){
+        searchStart(0)
+    });
+
+    function searchStart(index){
+        console.log("index : " + index);
+        $.get("/api/adminuser?page="+index, function(response){
+            console.dir(response);
+
+            indexBtn = [];
+            pagination = response.pagination;
+
+            //전체 페이지
+            showPage.totalElements = pagination.currentElements;
+            showPage.currentPage = pagination.currentPage + 1; //page가 0부터 시작해서 1플러스 해주기
+
+            //검색 데이터
+            itemList.itemList = response.data;
+
+
+        });
+    }
+
+})(jQuery);
